9c54a9bac46921125ee90ac2c8021c37fa26116d
Dan Waleke <dan@nowkw.com>
2022-01-27 20:32:02 -0500

    Converted to work with Visual Studio 2022.

--- MuPDF/Properties/AssemblyInfo.cs
+++ MuPDF/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MuPDF .NET Wrapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Quicken Loans")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MuPDF .NET Wrapper")]
-[assembly: AssemblyCopyright("Copyright © Quicken Loans 2011-2012")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
